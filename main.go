1f40f7936566b021562972e17244a1253d017ab1
juancrfig <juancrfig@gmail.com>
2025-11-13 11:49:05 -0500

    Read 8 bytes of a file

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"io"
+	"fmt"
+)
 
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	// Basic debugging config 
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatalf("Error while opening the file: %v", err)
+	}
+	defer file.Close()
+
+	var buffer = make([]byte, 8)
+	for {
+		n, err := file.Read(buffer)
+
+		if err == io.EOF {
+			break
+		}
+
+		if n < 8 {
+			fmt.Printf("read: %s\n", string(buffer[:n]))
+		} else {
+			fmt.Printf("read: %s\n", string(buffer))
+		}
+	}
 }
