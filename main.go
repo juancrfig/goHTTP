e378621f5d863af5d9ab28c0c2be222c3122f825
Juan Figueroa <juancrfig@gmail.com>
2025-11-17 23:03:57 -0500

    Reading lines from file

--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"os"
-	
-	"github.com/juancrfig/goHTTP/utils"
 )
-
 
 func main() {
 
-	file, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatalf("Error while opening the file: %v\n", err)
 	}
-	defer file.Close()
 
-	err = utils.PrintLinesFromFile(file, nil)
-
-	if err != nil {
-		log.Fatalf("Error while getting lines string: %v\n", err)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		leftover := &bytes.Buffer{}
+		for {
+			buf := make([]byte, 8)
+			n, err := f.Read(buf)
+
+			buf = buf[:n]
+			if i := bytes.IndexByte(buf, '\n'); i != -1 {
+				leftover.WriteString(string(buf[:i]))
+				ch <- leftover.String()
+
+				leftover.Reset()
+
+				leftover.WriteString(string(buf[i+1:]))
+				continue
+
+			}
+			//fmt.Printf("Writing '%s' to leftover: %s\n", string(buf), leftover.String())
+			leftover.WriteString(string(buf))
+
+			if err != nil {
+				break
+			}
+		}
+
+	}()
+
+	return ch
 }
